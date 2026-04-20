3e0715c325ee0eabf01e46bb4b607bcf562bb428
CarnellN <crnllngure@gmail.com>
2026-04-20 12:09:41 +0300

    loops

---
+++ practise/01_variables.py
@@ -0,0 +1,17 @@
+print("Hello Python!")
+
+name = "Paul"
+name2 = 'Paul'
+num = 10
+balance = 100.50
+flag = True
+
+print(id(name))
+print(id(name2))
+print(type(num))
+print(type(balance))
+print(type(flag))
+
+for i in range(1, 19, 5):
+    if i % 2 == 0:
+        print(i)
